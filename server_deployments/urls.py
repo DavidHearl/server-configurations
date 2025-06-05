6562e3626c302bd8f16f61ae62138a3f32a07d9b
DavidHearl <david.hearl@outlook.com>
2025-06-05 22:16:16 +0100

    updated the comonents template to include the other components

--- server_deployments/urls.py
+++ server_deployments/urls.py
@@ -5,9 +5,47 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('components/', views.components, name='components'),  # Added trailing slash
     path('components', views.components, name='components_no_slash'),  # Also match without slash
+    
+    # CPU routes
     path('components/add-cpu/', views.add_cpu, name='add_cpu'),
     path('components/edit-cpu/<int:cpu_id>/', views.edit_cpu, name='edit_cpu'),
     path('components/delete-cpu/<int:cpu_id>/', views.delete_cpu, name='delete_cpu'),
+    
+    # RAM routes
+    path('components/add-ram/', views.add_ram, name='add_ram'),
+    path('components/edit-ram/<int:ram_id>/', views.edit_ram, name='edit_ram'),
+    path('components/delete-ram/<int:ram_id>/', views.delete_ram, name='delete_ram'),
+    
+    # Motherboard routes
+    path('components/add-motherboard/', views.add_motherboard, name='add_motherboard'),
+    path('components/edit-motherboard/<int:motherboard_id>/', views.edit_motherboard, name='edit_motherboard'),
+    path('components/delete-motherboard/<int:motherboard_id>/', views.delete_motherboard, name='delete_motherboard'),
+    
+    # NIC routes
+    path('components/add-nic/', views.add_nic, name='add_nic'),
+    path('components/edit-nic/<int:nic_id>/', views.edit_nic, name='edit_nic'),
+    path('components/delete-nic/<int:nic_id>/', views.delete_nic, name='delete_nic'),
+    
+    # PSU routes
+    path('components/add-psu/', views.add_psu, name='add_psu'),
+    path('components/edit-psu/<int:psu_id>/', views.edit_psu, name='edit_psu'),
+    path('components/delete-psu/<int:psu_id>/', views.delete_psu, name='delete_psu'),
+    
+    # Case routes
+    path('components/add-case/', views.add_case, name='add_case'),
+    path('components/edit-case/<int:case_id>/', views.edit_case, name='edit_case'),
+    path('components/delete-case/<int:case_id>/', views.delete_case, name='delete_case'),
+    
+    # Storage routes
+    path('components/add-storage/', views.add_storage, name='add_storage'),
+    path('components/edit-storage/<int:storage_id>/', views.edit_storage, name='edit_storage'),
+    path('components/delete-storage/<int:storage_id>/', views.delete_storage, name='delete_storage'),
+    
+    # Rack routes
+    path('components/add-rack/', views.add_rack, name='add_rack'),
+    path('components/edit-rack/<int:rack_id>/', views.edit_rack, name='edit_rack'),
+    path('components/delete-rack/<int:rack_id>/', views.delete_rack, name='delete_rack'),
+    
     path('databases/', views.databases, name='databases'),  # Added trailing slash
     path('databases', views.databases, name='databases_no_slash'),  # Also match without slash
 ]
--- server_deployments/views.py
+++ server_deployments/views.py
@@ -9,25 +9,90 @@ def home(request):
 
 def components(request):
     """View for displaying and adding components"""
+    # Get all component types
     cpus = CPU.objects.all()
-    form = CPUForm()
+    rams = RAM.objects.all()
+    motherboards = Motherboard.objects.all()
+    nics = NIC.objects.all()
+    psus = PSU.objects.all()
+    cases = Case.objects.all()
+    storage_devices = StorageDevice.objects.all()
+    racks = Rack.objects.all()
+    
+    # Create forms for each component type
+    cpu_form = CPUForm()
+    ram_form = RAMForm()
+    motherboard_form = MotherboardForm()
+    nic_form = NICForm()
+    psu_form = PSUForm()
+    case_form = CaseForm()
+    storage_form = StorageDeviceForm()
+    rack_form = RackForm()
     
     # Check if we're in edit mode
     edit_mode = False
     edit_id = None
+    component_type = None
     
+    # Check for edit parameters in the URL
     if request.GET.get('edit'):
         edit_id = request.GET.get('edit')
+        component_type = request.GET.get('type', 'cpu')  # Default to CPU if not specified
         edit_mode = True
-        cpu = get_object_or_404(CPU, id=edit_id)
-        form = CPUForm(instance=cpu)
+        
+        # Based on component type, get the correct instance and form
+        if component_type == 'cpu':
+            cpu = get_object_or_404(CPU, id=edit_id)
+            cpu_form = CPUForm(instance=cpu)
+        elif component_type == 'ram':
+            ram = get_object_or_404(RAM, id=edit_id)
+            ram_form = RAMForm(instance=ram)
+        elif component_type == 'motherboard':
+            motherboard = get_object_or_404(Motherboard, id=edit_id)
+            motherboard_form = MotherboardForm(instance=motherboard)
+        elif component_type == 'nic':
+            nic = get_object_or_404(NIC, id=edit_id)
+            nic_form = NICForm(instance=nic)
+        elif component_type == 'psu':
+            psu = get_object_or_404(PSU, id=edit_id)
+            psu_form = PSUForm(instance=psu)
+        elif component_type == 'case':
+            case = get_object_or_404(Case, id=edit_id)
+            case_form = CaseForm(instance=case)
+        elif component_type == 'storage':
+            storage = get_object_or_404(StorageDevice, id=edit_id)
+            storage_form = StorageDeviceForm(instance=storage)
+        elif component_type == 'rack':
+            rack = get_object_or_404(Rack, id=edit_id)
+            rack_form = RackForm(instance=rack)
     
     context = {
+        # All component data
         'cpus': cpus,
-        'form': form,
+        'rams': rams,
+        'motherboards': motherboards,
+        'nics': nics,
+        'psus': psus,
+        'cases': cases,
+        'storage_devices': storage_devices,
+        'racks': racks,
+        
+        # All forms
+        'cpu_form': cpu_form,
+        'ram_form': ram_form,
+        'motherboard_form': motherboard_form,
+        'nic_form': nic_form,
+        'psu_form': psu_form,
+        'case_form': case_form,
+        'storage_form': storage_form,
+        'rack_form': rack_form,
+        
+        # Edit mode info
         'edit_mode': edit_mode,
-        'edit_id': edit_id
+        'edit_id': edit_id,
+        'component_type': component_type
     }
+    
     return render(request, 'server_deployments/components.html', context)
 
 def add_cpu(request):
@@ -35,7 +100,6 @@ def add_cpu(request):
         form = CPUForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('components')
     return redirect('components')
 
 def edit_cpu(request, cpu_id):
@@ -45,14 +109,178 @@ def edit_cpu(request, cpu_id):
         if form.is_valid():
             form.save()
             return redirect('components')
-    return redirect(f'/components/?edit={cpu_id}')
-
+    return redirect(f'/components/?edit={cpu_id}&type=cpu&tab=cpu-tab')
 
 def delete_cpu(request, cpu_id):
     cpu = get_object_or_404(CPU, id=cpu_id)
     if request.method == 'POST':
         cpu.delete()
-        return redirect('components')
+    return redirect('components')
+
+def add_ram(request):
+    if request.method == 'POST':
+        form = RAMForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return redirect('components')
+
+def edit_ram(request, ram_id):
+    ram = get_object_or_404(RAM, id=ram_id)
+    if request.method == 'POST':
+        form = RAMForm(request.POST, instance=ram)
+        if form.is_valid():
+            form.save()
+            return redirect('components')
+    return redirect(f'/components/?edit={ram_id}&type=ram&tab=ram-tab')
+
+def delete_ram(request, ram_id):
+    ram = get_object_or_404(RAM, id=ram_id)
+    if request.method == 'POST':
+        ram.delete()
+    return redirect('components')
+
+
+# Motherboard CRUD operations
+def add_motherboard(request):
+    if request.method == 'POST':
+        form = MotherboardForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return redirect('components')
+
+def edit_motherboard(request, motherboard_id):
+    motherboard = get_object_or_404(Motherboard, id=motherboard_id)
+    if request.method == 'POST':
+        form = MotherboardForm(request.POST, instance=motherboard)
+        if form.is_valid():
+            form.save()
+            return redirect('components')
+    return redirect(f'/components/?edit={motherboard_id}&type=motherboard&tab=motherboard-tab')
+
+def delete_motherboard(request, motherboard_id):
+    motherboard = get_object_or_404(Motherboard, id=motherboard_id)
+    if request.method == 'POST':
+        motherboard.delete()
+    return redirect('components')
+
+
+# NIC CRUD operations
+def add_nic(request):
+    if request.method == 'POST':
+        form = NICForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return redirect('components')
+
+def edit_nic(request, nic_id):
+    nic = get_object_or_404(NIC, id=nic_id)
+    if request.method == 'POST':
+        form = NICForm(request.POST, instance=nic)
+        if form.is_valid():
+            form.save()
+            return redirect('components')
+    return redirect(f'/components/?edit={nic_id}&type=nic&tab=nic-tab')
+
+def delete_nic(request, nic_id):
+    nic = get_object_or_404(NIC, id=nic_id)
+    if request.method == 'POST':
+        nic.delete()
+    return redirect('components')
+
+
+# PSU CRUD operations
+def add_psu(request):
+    if request.method == 'POST':
+        form = PSUForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return redirect('components')
+
+def edit_psu(request, psu_id):
+    psu = get_object_or_404(PSU, id=psu_id)
+    if request.method == 'POST':
+        form = PSUForm(request.POST, instance=psu)
+        if form.is_valid():
+            form.save()
+            return redirect('components')
+    return redirect(f'/components/?edit={psu_id}&type=psu&tab=psu-tab')
+
+def delete_psu(request, psu_id):
+    psu = get_object_or_404(PSU, id=psu_id)
+    if request.method == 'POST':
+        psu.delete()
+    return redirect('components')
+
+
+# Case CRUD operations
+def add_case(request):
+    if request.method == 'POST':
+        form = CaseForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return redirect('components')
+
+def edit_case(request, case_id):
+    case = get_object_or_404(Case, id=case_id)
+    if request.method == 'POST':
+        form = CaseForm(request.POST, instance=case)
+        if form.is_valid():
+            form.save()
+            return redirect('components')
+    return redirect(f'/components/?edit={case_id}&type=case&tab=case-tab')
+
+def delete_case(request, case_id):
+    case = get_object_or_404(Case, id=case_id)
+    if request.method == 'POST':
+        case.delete()
+    return redirect('components')
+
+
+# Storage CRUD operations
+def add_storage(request):
+    if request.method == 'POST':
+        form = StorageDeviceForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return redirect('components')
+
+def edit_storage(request, storage_id):
+    storage = get_object_or_404(StorageDevice, id=storage_id)
+    if request.method == 'POST':
+        form = StorageDeviceForm(request.POST, instance=storage)
+        if form.is_valid():
+            form.save()
+            return redirect('components')
+    return redirect(f'/components/?edit={storage_id}&type=storage&tab=storage-tab')
+
+def delete_storage(request, storage_id):
+    storage = get_object_or_404(StorageDevice, id=storage_id)
+    if request.method == 'POST':
+        storage.delete()
+    return redirect('components')
+
+
+# Rack CRUD operations
+def add_rack(request):
+    if request.method == 'POST':
+        form = RackForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return redirect('components')
+
+def edit_rack(request, rack_id):
+    rack = get_object_or_404(Rack, id=rack_id)
+    if request.method == 'POST':
+        form = RackForm(request.POST, instance=rack)
+        if form.is_valid():
+            form.save()
+            return redirect('components')
+    return redirect(f'/components/?edit={rack_id}&type=rack&tab=rack-tab')
+
+def delete_rack(request, rack_id):
+    rack = get_object_or_404(Rack, id=rack_id)
+    if request.method == 'POST':
+        rack.delete()
     return redirect('components')
 
 
